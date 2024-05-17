96c07a4d7276999bb29f8e12420ca29a55c3193f
edward.ortegamo <edward.ortegamo@gmail.com>
2024-05-17 13:12:10 -0500

    Pag 70

--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -8,12 +8,22 @@ import * as dataRaw from '../../../data/tracks.json';
   styleUrl: './play-list-body.component.css'
 })
 export class PlayListBodyComponent implements OnInit{
-  //Añadimos el track con el array TrackModel
+  //NUEVO Añadimos el track con el array TrackModel
   tracks: TrackModel[] = []
+  optionSort: { property: string | null, order: string} = { property: null, order: 'asc'}
   constructor() { }
   ngOnInit(): void {
     //Usamos el dataRaw que importamos
   const {data}: any = (dataRaw as any).default
   this.tracks = data;
+}
+//NUEVO Se agrega la funcion changeSort para ordenar nombres, albuns
+changeSort(property: string): void{
+  const {order} = this.optionSort
+  this.optionSort = {
+    property,
+    order: order === 'asc' ? 'desc' : 'asc'
+  }
+
 }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,26 +7,34 @@ import { CardPlayerComponent } from './components/card-player/card-player.compon
 import { SectionGenericComponent } from './components/section-generic/section-generic.component';
 import { PlayListHeaderComponent } from './components/play-list-header/play-list-header.component';
 import { PlayListBodyComponent } from './components/play-list-body/play-list-body.component';
+import { RouterModule } from '@angular/router';
+import { OrderListPipe } from './pipe/order-list.pipe';
+import { ImgBrokenDirective } from './directives/img-broken.directive';
 
 @NgModule({
-  declarations: [
-    SideBarComponent, 
-    MediaPlayeComponent, 
-    HeaderUserComponent,
-    CardPlayerComponent,
-    SectionGenericComponent,
-    PlayListHeaderComponent, // Declara el componente
-    PlayListBodyComponent // Declara el componente
-  ],
-  imports: [CommonModule],
-  exports: [
-    SideBarComponent, 
-    MediaPlayeComponent, 
-    HeaderUserComponent,
-    CardPlayerComponent,
-    SectionGenericComponent, 
-    PlayListHeaderComponent, //Se añade
-    PlayListBodyComponent
-  ],
+    declarations: [
+        SideBarComponent,
+        MediaPlayeComponent,
+        HeaderUserComponent,
+        CardPlayerComponent,
+        SectionGenericComponent,
+        PlayListHeaderComponent, // Declara el componente
+        PlayListBodyComponent, // Declara el componente
+    ],
+    exports: [
+        SideBarComponent,
+        MediaPlayeComponent,
+        HeaderUserComponent,
+        CardPlayerComponent,
+        SectionGenericComponent,
+        PlayListHeaderComponent, //Se añade
+        PlayListBodyComponent,
+        OrderListPipe //Se agrega
+    ],
+    imports: [CommonModule,
+        RouterModule, //Se agrega para usar la directiva RouterLink
+        OrderListPipe, //NUEVO
+        ImgBrokenDirective //NUEVO
+       ]
 })
 export class SharedModule {}
